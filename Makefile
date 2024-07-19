72ecb778fdd8b93c5f41748b1d55d9aef2d35fd0
gw-shinohara <shinohara@globalwalkers.co.jp>
2024-07-19 18:40:42 +0900

    [Add] make file

---
+++ Makefile
@@ -0,0 +1,18 @@
+#!/bin/bash
+.PHONY build_venv delete_venv install train :
+
+build_venv:
+	python3 -m venv .venv
+
+delete_venv:
+	sudo rm -r .venv
+
+install: build_venv
+	. .venv/bin/activate && \
+	python3 -m pip list && \
+	python3 -m pip install -U pip &&\
+	python3 -m pip install -r ./rtdetr_pytorch/requirements.txt && \
+	deactivate
+train:
+	. .venv/bin/activate && \
+	bash rtdetr_pytorch/tools/train.sh
--- rtdetr_pytorch/tools/train.sh
+++ rtdetr_pytorch/tools/train.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-python3 train.py \
--c /home/gw-shinohara/Documents/csp_drone_detection/RT-DETR/rtdetr_pytorch/configs/dataset/thermal_dogs_and_people_detection.yml \
+pushd ./rtdetr_pytorch
+python3 tools/train.py \
+-c /home/gw-shinohara/Documents/csp_drone_detection/RT-DETR/rtdetr_pytorch/configs/rtdetr/rtdetr_r18vd_6x_thermal_dog_peple.yml \
 -t https://github.com/lyuwenyu/storage/releases/download/v0.1/rtdetr_r18vd_5x_coco_objects365_from_paddle.pth
+popd
